f6de911ee70c77d46a18f22eaeb1da824c947f92
pa1007 <pa.fourriere@pa1007.fr>
2018-12-21 22:48:03 +0100

    Create game

---
+++ src/main/java/fr/pa1007/chess/game/Game.java
@@ -0,0 +1,25 @@
+package fr.pa1007.chess.game;
+
+import fr.pa1007.chess.chessman.ChessMan;
+import fr.pa1007.chess.chessman.ChessManType;
+import fr.pa1007.chess.utils.Pattern;
+import fr.pa1007.chess.utils.Place;
+import fr.pa1007.chess.utils.Player;
+
+public class Game {
+
+    public Game() {
+    }
+
+    public int remaining(ChessManType chess) {
+        return 0;
+    }
+
+    public Place getPlace(ChessMan man) {
+        return null;
+    }
+
+    public static Pattern getPatternFrom(Player player) {
+        return null;
+    }
+}
---
+++ src/main/java/fr/pa1007/chess/game/GameType.java
@@ -0,0 +1,20 @@
+package fr.pa1007.chess.game;
+
+public enum GameType {
+
+    /**
+     * The type of the IA vs IA
+     */
+    IAVSIA,
+
+    /**
+     * The type of the player vs IA
+     */
+    PLYAERVSIA,
+
+    /**
+     * The type of the Player vs Player (Comming soon)
+     */
+    PLAYERVSPLAYER
+
+}
