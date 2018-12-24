a73ea88ba45f62c7a1b3aad0a6532c21a539382a
pa1007 <pa.fourriere@pa1007.fr>
2018-12-24 16:37:02 +0100

    Add some useful things to help in Game

--- src/main/java/fr/pa1007/chess/game/Game.java
+++ src/main/java/fr/pa1007/chess/game/Game.java
@@ -5,10 +5,35 @@ import fr.pa1007.chess.chessman.ChessManType;
 import fr.pa1007.chess.utils.Pattern;
 import fr.pa1007.chess.utils.Place;
 import fr.pa1007.chess.utils.Player;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Game {
 
-    public Game() {
+    private Map<Player, List<ChessMan>> graphic;
+    private Map<Player, List<ChessMan>> deadMap;
+
+    /**
+     * Constructor for copping a game
+     *
+     * @param game the game to copy from
+     */
+    public Game(Game game) {
+
+    }
+
+
+    /**
+     * @param graphicRepresentation all the pieces to use
+     */
+    public Game(Map<Player, List<ChessMan>> graphicRepresentation) {
+        this.graphic = graphicRepresentation;
+        this.deadMap = new HashMap<>();
+    }
+
+    public Map<Player, List<ChessMan>> getGraphic() {
+        return graphic;
     }
 
     public int remaining(ChessManType chess) {
@@ -17,6 +42,19 @@ public class Game {
 
     public Place getPlace(ChessMan man) {
         return null;
+    }
+
+    public Player getOtherPlayer(Player player) {
+        for (Player player1 : graphic.keySet()) {
+            if (player != player1) {
+                return player1;
+            }
+        }
+        return null;
+    }
+
+    public Map<Player, List<ChessMan>> getDead() {
+        return this.deadMap;
     }
 
     public static Pattern getPatternFrom(Player player) {
