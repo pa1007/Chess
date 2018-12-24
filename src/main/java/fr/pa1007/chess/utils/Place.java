85ede0e285b9cb6cb8538e3d526bae8f9de6e5a1
pa1007 <pa.fourriere@pa1007.fr>
2018-12-24 16:34:52 +0100

    Add easy creation for Places

--- src/main/java/fr/pa1007/chess/utils/Place.java
+++ src/main/java/fr/pa1007/chess/utils/Place.java
@@ -1,5 +1,9 @@
 package fr.pa1007.chess.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Place {
 
 
@@ -10,8 +14,72 @@ public class Place {
      */
     private String name;
 
+
+    /**
+     * The column character.
+     *
+     * @since 1.0
+     */
+    private String column;
+    /**
+     * The row number.
+     *
+     * @since 1.0
+     */
+    private int    row;
+
+
     public Place(String s) {
-        this.name = s;
+        this.name = s.toUpperCase();
+        generatePlace();
+    }
+
+    public Place(String name, int row) {
+        this.column = name.toUpperCase();
+        this.row = row;
+        this.name = column + this.row;
+    }
+
+    /**
+     * @return The column character.
+     *
+     * @since 1.0
+     */
+    public String getColumn() {
+        return this.column;
+    }
+
+    /**
+     * Sets the <code>column</code> field.
+     *
+     * @param column The column character.
+     *
+     * @since 1.0
+     */
+    public void setColumn(String column) {
+        this.name = this.name.replaceFirst(this.column, column);
+        this.column = column;
+    }
+
+    /**
+     * @return The row number.
+     *
+     * @since 1.0
+     */
+    public int getRow() {
+        return this.row;
+    }
+
+    /**
+     * Sets the <code>row</code> field.
+     *
+     * @param row The row number.
+     *
+     * @since 1.0
+     */
+    public void setRow(int row) {
+        this.name = this.name.replaceFirst(String.valueOf(this.row), String.valueOf(row));
+        this.row = row;
     }
 
     /**
@@ -32,9 +100,124 @@ public class Place {
      */
     public void setName(String name) {
         this.name = name;
+        generatePlace();
     }
 
-    public static void transformGridToPlace() {
+    /**
+     * This method give you the current place with the moved of row and column add in parameters <br>
+     * The column is between "A" and "H", the row is between 1 and 8
+     *
+     * @param row    the amount of row to move from max 7
+     * @param column the amount of column to move from max 7
+     *
+     * @return a new place with the moved column and row or null if the place is outside the board
+     */
+    public Place more(int row, int column) {
+        int tempR = this.row + row;
+        int tempC = (int) this.column.toLowerCase().charAt(0) + column;
+        if (tempR > 8 || tempR < 1) {
+            System.out.println(tempR);
+            return null;
+        }
+        if (tempC > 104 || tempC < 97) {
+            System.out.println(tempC);
+            return null;
+        }
+        return new Place(String.valueOf(Character.valueOf((char) tempC)), tempR);
+    }
 
+    public int getColumnNumber() {
+        return column.toLowerCase().charAt(0) - 'a';
+    }
+
+    public Place[] getLines() {
+        Place[] places     = new Place[8 * 4];
+        int     placeCount = 0;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(0, i);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        placeCount++;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(i, 0);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        placeCount++;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(0, -i);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        placeCount++;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(-i, 0);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        return places;
+    }
+
+    public Place[] getDiagonal() {
+        Place[] places     = new Place[8 * 4];
+        int     placeCount = 0;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(i, i);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        placeCount++;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(i, -i);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        placeCount++;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(-i, i);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        placeCount++;
+        for (int i = 1; i != 8; i++) {
+            places[placeCount] = more(-i, -i);
+            placeCount++;
+        }
+        places[placeCount] = new Place("P6");
+        return places;
+    }
+
+    public Place[] getPlaceAround() {
+        Place[] places = new Place[8];
+        places[0] = more(-1, -1);
+        places[1] = more(-1, 1);
+        places[2] = more(-1, 0);
+        places[3] = more(0, -1);
+        places[4] = more(0, 1);
+        places[5] = more(1, -1);
+        places[6] = more(1, 0);
+        places[7] = more(1, 1);
+        return places;
+    }
+
+    public boolean is(String p6) {
+        return name.equals(p6.toUpperCase());
+    }
+
+    private void generatePlace() {
+        column = String.valueOf(name.charAt(0));
+        row = Character.digit(name.charAt(1), 10);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static Place[] getAll(Place[] lines, Place[] diagonal) {
+        List<Place> places = new ArrayList<>(Arrays.asList(lines));
+        places.addAll(Arrays.asList(diagonal));
+        return places.toArray(new Place[0]);
     }
 }
