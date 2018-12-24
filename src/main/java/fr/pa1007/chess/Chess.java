77060db1c0dc48a90701cef1b6e6ead540af7f09
pa1007 <pa.fourriere@pa1007.fr>
2018-12-24 16:33:10 +0100

    Add easy error display for user

--- src/main/java/fr/pa1007/chess/Chess.java
+++ src/main/java/fr/pa1007/chess/Chess.java
@@ -1,12 +1,17 @@
 package fr.pa1007.chess;
 
 import fr.pa1007.chess.controller.GameController;
+import fr.pa1007.chess.utils.exception.ChessOutOfBoundException;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class Chess extends Application {
 
@@ -19,12 +24,29 @@ public class Chess extends Application {
         GameController controllerMainTable = loader.getController();
         Stage          stage               = new Stage();
         stage.setScene(new Scene(pane));
-        stage.setTitle("Main | Snake");
+        stage.setTitle("Main | Chess");
+        stage.setResizable(false);
         stage.setOnCloseRequest((windowEvent) -> {
             Platform.exit();
             System.exit(0);
         });
         stage.show();
         controllerMainTable.init();
+    }
+
+    public static void createExceptionAlert(ChessOutOfBoundException e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error Dialog");
+        alert.setHeaderText(e.getMessage());
+        StringWriter sw = new StringWriter();
+        PrintWriter  pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        TextArea textArea = new TextArea(sw.toString());
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        alert.getDialogPane().setExpandableContent(textArea);
+        alert.show();
     }
 }
