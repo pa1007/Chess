dd0cdb98060c5cb00cffc82bfcea514aa4c2c4bb
pa1007 <pa.fourriere@pa1007.fr>
2018-12-24 16:35:43 +0100

    Create a handler for movement

---
+++ src/main/java/fr/pa1007/chess/handler/ChessMoveEventHandler.java
@@ -0,0 +1,76 @@
+package fr.pa1007.chess.handler;
+
+import fr.pa1007.chess.chessman.ChessMan;
+import fr.pa1007.chess.game.Game;
+import fr.pa1007.chess.utils.Place;
+import fr.pa1007.chess.utils.Player;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChessMoveEventHandler implements EventHandler<MouseEvent> {
+
+    private       Place    place;
+    private       ChessMan chessMan;
+    private final GridPane grid;
+    private final Game     game;
+
+    public ChessMoveEventHandler(Place place, ChessMan cM, GridPane plane, Game game) {
+        this.place = place;
+        this.chessMan = cM;
+        this.grid = plane;
+        this.game = game;
+    }
+
+    /**
+     * Invoked when a specific event of the type for which this handler is
+     * registered happens.
+     *
+     * @param event the event which occurred
+     */
+    @Override
+    public void handle(MouseEvent event) {
+        int  row    = place.getRow() - 1;
+        int  column = place.getColumnNumber();
+        Node nodeT  = (Node) event.getTarget();
+        if (nodeT.getId().contains("eat")) {
+            System.out.println("Here");
+            Player player = game.getOtherPlayer(chessMan.getPlayer());
+            for (ChessMan man : game.getGraphic().get(player)) {
+                Rectangle rec = man.getGraphicRep();
+                Integer   cI  = GridPane.getColumnIndex(rec);
+                Integer   rI  = GridPane.getRowIndex(rec);
+                int       c   = cI == null ? 0 : cI;
+                int       r   = rI == null ? 0 : rI;
+                if (c == column && r == row) {
+                    System.out.println("Found");
+                    game.getGraphic().get(player).remove(rec);
+                    if (!game.getDead().containsKey(player)) {
+                        List<ChessMan> lis = new ArrayList<>();
+                        lis.add(man);
+                        game.getDead().put(player, lis);
+                    }
+                    else {
+                        game.getDead().get(player).add(man);
+                    }
+                    grid.getChildren().remove(rec);
+                    break;
+                }
+            }
+        }
+
+        grid.getChildren().removeIf(node -> node.getId().contains("help"));
+        grid.getChildren().removeIf(node -> node == chessMan.getGraphicRep());
+        grid.add(chessMan.getGraphicRep(), column, row);
+        chessMan.place().setRow(place.getRow());
+        chessMan.place().setColumn(place.getColumn());
+        int i = chessMan.movementNumber() + 1;
+        chessMan.setMoveNumber(i);
+    }
+
+
+}
