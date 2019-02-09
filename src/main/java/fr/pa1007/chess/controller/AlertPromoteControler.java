d8e83026d991721d87e7285c3c556a103268dde6
pa1007 <pa.fourriere@sfr.fr>
2019-02-09 23:31:43 +0100

    Add graphic interface for promoting a paw

---
+++ src/main/java/fr/pa1007/chess/controller/AlertPromoteControler.java
@@ -0,0 +1,61 @@
+package fr.pa1007.chess.controller;
+
+import fr.pa1007.chess.chessman.ChessManType;
+import fr.pa1007.chess.chessman.pieces.Paw;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class AlertPromoteControler {
+
+    public  ImageView queenI;
+    public  ImageView bishopI;
+    public  ImageView knightI;
+    public  ImageView rookI;
+    private Paw       paw;
+
+    public void init(Paw paw) {
+        this.paw = paw;
+        queenI.setImage(new Image("fr/pa1007/chess/display/"
+                                  + paw.getPlayer().getTeam().toLowerCase()
+                                  + "/Queen.png"));
+        bishopI.setImage(new Image("fr/pa1007/chess/display/"
+                                   + paw.getPlayer().getTeam().toLowerCase()
+                                   + "/Bishop.png"));
+        knightI.setImage(new Image("fr/pa1007/chess/display/"
+                                   + paw.getPlayer().getTeam().toLowerCase()
+                                   + "/Knight.png"));
+        rookI.setImage(new Image("fr/pa1007/chess/display/"
+                                 + paw.getPlayer().getTeam().toLowerCase()
+                                 + "/Rook.png"));
+
+    }
+
+    public void queenHandler(MouseEvent mouseEvent) {
+        paw.promote(ChessManType.QUEEN);
+        closeStage(mouseEvent);
+    }
+
+    public void rookHandler(MouseEvent mouseEvent) {
+        paw.promote(ChessManType.ROOKS);
+        closeStage(mouseEvent);
+    }
+
+    public void bishiopHandler(MouseEvent mouseEvent) {
+        paw.promote(ChessManType.BISHOP);
+        closeStage(mouseEvent);
+    }
+
+    public void knightHandler(MouseEvent mouseEvent) {
+        paw.promote(ChessManType.KNIGHT);
+        closeStage(mouseEvent);
+    }
+
+    private void closeStage(MouseEvent event) {
+        Node  source = (Node) event.getSource();
+        Stage stage  = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+}
