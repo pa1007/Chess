3b27d206cb8a715ef2d242da0dbb7167a65c07ec
pa1007 <pa.fourriere@sfr.fr>
2019-02-09 18:45:47 +0100

    Add more method and variable to this abstract class

--- src/main/java/fr/pa1007/chess/chessman/AbstractChessMan.java
+++ src/main/java/fr/pa1007/chess/chessman/AbstractChessMan.java
@@ -1,6 +1,7 @@
 package fr.pa1007.chess.chessman;
 
 import fr.pa1007.chess.game.Game;
+import fr.pa1007.chess.utils.MovePattern;
 import fr.pa1007.chess.utils.Place;
 import fr.pa1007.chess.utils.Player;
 import javafx.scene.shape.Rectangle;
@@ -35,13 +36,24 @@ public abstract class AbstractChessMan implements ChessMan {
 
 
     /**
+     * The move number of the piece, get if with <code>this.move</code>
+     */
+    protected int move;
+
+
+    /**
+     * The move pattern of the piece, get if with <code>this.movePattern</code>
+     */
+    protected MovePattern movePattern;
+
+    /**
      * this constructor is done for creating an identical constructor in every other piece, this help to have a game instance, the graphic representation
      * , the place and a player
      *
-     * @param chessGame the instance of the {@link fr.pa1007.chess.game.Game Game}
+     * @param chessGame the instance of the {@link Game Game}
      * @param graph     the graphic representation
-     * @param place     the {@link fr.pa1007.chess.utils.Place place} of the piece
-     * @param player    the {@link fr.pa1007.chess.utils.Place player} who owns the piece
+     * @param place     the {@link Place place} of the piece
+     * @param player    the {@link Place player} who owns the piece
      */
     public AbstractChessMan(Game chessGame, Rectangle graph, Place place, Player player) {
         Objects.requireNonNull(chessGame);
@@ -62,6 +74,28 @@ public abstract class AbstractChessMan implements ChessMan {
     @Override
     public Rectangle getGraphicRep() {
         return graph;
+    }
+
+
+    @Override
+    public int movementNumber() {
+        return move;
+    }
+
+    @Override
+    public void setMoveNumber(int i) {
+        move = i;
+    }
+
+    @Override
+    public Place place() {
+        return this.place;
+    }
+
+
+    @Override
+    public MovePattern movePattern() {
+        return this.movePattern;
     }
 
 }
